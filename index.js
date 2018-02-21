897783de1d114a422feb172a7ad06ad0b2cacfdf
Coty Condry <ccondry@cisco.com>
2018-02-20 18:07:05 -0600

    got sparky working with new sparky-bot UI

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ require('dotenv').load();
 
 // Needed to get around self signed certs
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-// API.AI token
-const APIAI_TOKEN = process.env.APIAI_TOKEN;
 // Node includes
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -23,9 +21,6 @@ const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env)
 })
 
-// init api.ai client
-// const apiaiApp = apiai(APIAI_TOKEN)
-
 // Begin ECE
 var eGainLibrarySettings = require('./egain/egainNode').eGainLibrarySettings;
 var myLibrarySettings = new eGainLibrarySettings();
@@ -39,10 +34,7 @@ var file = "";
 var eGainLibrary = require('./egain/egainNode').eGainLibrary;
 var myLibrary = new eGainLibrary(myLibrarySettings);
 myLibrary.CORSHost = process.env.ECE_HOST;
-/* Now create an instance of the Chat Object */
-var myChat = new myLibrary.Chat();
-/* Next get the event handlers for chat. It is mandatory to provide definition for the mandatory event handlers before initializing chat */
-var myEventHandlers = myChat.GetEventHandlers();
+
 
 // ECE Parameters
 
@@ -70,15 +62,29 @@ app.get('/version', function (req, res) {
 
 // sessions storage
 let sessions = {}
-
+//
+let tokens = {
+  'cumulus-finance': '1c4d3b458b3f4109bec0b38f792cfc46',
+  'sparky-retail': 'a2083e974dc84b599e86124fca44a9e3'
+}
 // get new session ID for client
-app.get('/api/v1/session', (req, res) => {
+app.post('/api/v1/session', (req, res) => {
+  console.log('new session request', req.body)
+  // get api.ai token
+  let apiAiToken = req.body.apiAiToken || tokens[req.body.bot] || '1c4d3b458b3f4109bec0b38f792cfc46'
+
+  // create session and store in sessions global
   const sessionId = uuidv1()
-  // store session in sessions
   sessions[sessionId] = {
     id: sessionId,
     state: 'active',
-    messages: []
+    messages: [],
+    entryPointId: req.body.entryPointId || '1001',
+    phone: req.body.phone,
+    email: req.body.email,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    apiAiToken
   }
   // generate uuid and return to client
   res.status(200).send({sessionId})
@@ -94,7 +100,35 @@ app.post('/api/v1/messages', (req, res) => {
   if (sessionId && sessions[sessionId]) {
     // valid session
     const session = sessions[sessionId]
-    addCustomerMessage(session, req.body.text)
+    // is this chat escalated to an agent?
+    if (session.escalated) {
+      // send message to ECE agent
+      session.eceSession.SendMessageToAgent(req.body.text)
+      // add message to memory
+      session.messages.push({
+        text: req.body.text,
+        type: 'customer',
+        datetime: new Date().toJSON()
+      })
+      // check for command words
+      switch (req.body.text) {
+        case 'goodbye': {
+          // end ECE session
+          session.eceSession.End()
+          // tell user session ended
+          session.messages.push({
+            text: 'Your session with our expert has ended, but you can still chat with Sparky.',
+            type: 'system',
+            datetime: new Date().toJSON()
+          })
+          // remove escalated flag
+          session.escalated = false
+          break
+        }
+      }
+    } else {
+      addCustomerMessage(session, req.body.text)
+    }
 
     // return ACCEPTED
     return res.status(202).send()
@@ -123,7 +157,7 @@ async function processCustomerMessage (session, text) {
       method: 'POST',
       url: 'https://api.api.ai/v1/query',
       headers: {
-        'Authorization': `Bearer ${APIAI_TOKEN}`,
+        'Authorization': `Bearer ${session.apiAiToken}`,
         'Accept': `application/json`,
         'Content-Type': `application/json; charset=utf-8`
       },
@@ -137,7 +171,7 @@ async function processCustomerMessage (session, text) {
       },
       json: true
     })
-    console.log('api.ai response', response)
+    // console.log('api.ai response', response)
     // process the response text
     processAiResponse(session, response.result.fulfillment.speech)
 
@@ -246,105 +280,25 @@ app.post('/ai', (req, res) => {
 
 });
 
-/* Example browser alert when chat is connected */
-myEventHandlers.OnConnectSuccess = function (args) {
-  console.log('OnConnectSuccess', args)
-  var welcomeMessage = "You are now connected to a Cumulus Financial Expert.";
-  console.log("You are now connected to an Agent " + welcomeMessage);
 
-};
-/* Example browser alert when there is a connection failure */
-myEventHandlers.OnConnectionFailure = function (args) {
-  console.log('OnConnectionFailure', args)
-  // console.log('Oops! Something went wrong');
-};
-/* Example output of agent messages to a DIV named TransScript with jQuery */
-myEventHandlers.OnAgentMessageReceived = function (args) {
-  console.log('OnAgentMessageReceived', args)
-  console.log("Agent Message Received: " + args.Message);
-};
-/* Example output of system messages to the same DIV */
-myEventHandlers.OnSystemMessageReceived = function (args) {
-  console.log("System Message Received: " + args.Message);
-};
-/* Example browser console.log when an error occurs */
-myEventHandlers.OnErrorOccurred = function (args) {
-  console.log('Oops! Error Occurred' + args.toString());
-};
-/* Example browser console.log when agents are not available */
-myEventHandlers.OnAgentsNotAvailable = function (args) {
-  console.log('Sorry no agents available', args);
-};
-/* Example browser console.log when the chat is completed */
-myEventHandlers.OnConnectionComplete = function (args) {
-  console.log("Connection Complete!", args)
-};
-/* Example of adding message in transcript when customer attachment invite is sent to server */
-myEventHandlers.OnCustomerAttachmentNotificationSent = function(args){
-  console.log('OnCustomerAttachmentNotificationSent', args);
-  const message = "Waiting for agent to accept attachment"
-};
-/* Example of uploading attachment to chat server when agent accepts attachment invite */
-myEventHandlers.OnAttachmentAcceptedByAgent = function(attachmentAcceptedByAgentEventArgs){
-  file.uniqueFileId = attachmentAcceptedByAgentEventArgs.uniqueFileId;
-  myChat.UploadAttachment(file,attachmentAcceptedByAgentEventArgs.agentName);
-};
-
-/* Example of sending notification to chat server when customer accepts attachment invite */
-myEventHandlers.OnAttachmentInviteReceived = function(args){
-  var acceptBtn = document.createElement('input');
-  acceptBtn.type = "button";
-  acceptBtn.value="Accept";
-  acceptBtn.addEventListener('click', function(){
-    sendAcceptChatAttachmentNotification(args.Attachment);
-  });
-  // $('#messages ul').append( '<li><span class="systemmsg-chat">' + args.Attachment.AgentName + " has sent attachment "+args.Attachment.Name + '</span><div class="clear"></div></li>');
-  // $('#messages ul').append(acceptBtn);
-};
-
-/* Example of downloading file when attachment is fetched from server */
-myEventHandlers.OnGetAttachment = function(AgentAttachmentArgs){
-  if (typeof fileName !== 'undefine' && fileName !== null) {
-    if ((/\.(gif|jpg|jpeg|tiff|png)$/i).test(fileName)) {
-      myChat.GetAttachmentImage(AgentAttachmentArgs.fileId,AgentAttachmentArgs.uniqueFileId);
-    }
-    else{
-      var data = AgentAttachmentArgs.data;
-      var blob = new Blob([data]);
-      url = window.URL || window.webkitURL;
-      var fileUrl = url.createObjectURL(blob);
-      window.open(fileUrl);
-    }
-  }
-
-};
-/* Example of downloading file when attachment thumbnail is fetched from server */
-myEventHandlers.OnGetAttachmentImageThumbnail = function(thumbnailArgs){
-  var thumbnailElement = document.createElement('img');
-  thumbnailElement.src = thumbnailArgs.data;
-  $('#messages ul').append("<br />");
-  $('#messages ul').append(thumbnailElement);
-};
-function sendAcceptChatAttachmentNotification(attachment){
-  fileName = attachment.Name;
-  myChat.SendAcceptChatAttachmentNotification(attachment.Id,attachment.Name);
-  myChat.GetAttachment(attachment.Id);
-};
 
 function escalateIt (session) {
   /* Create the customer object */
-  var ChatEntryPointId = "1001";
-  var PhoneNumber = "2142336226";
-  var EmailAddress = "ccondry@cisco.com";
-  var FirstName = "Coty";
-  var LastName = "Condry";
+  // var ChatEntryPointId = "1001";
+  // var PhoneNumber = "2142336226";
+  // var EmailAddress = "ccondry@cisco.com";
+  // var FirstName = "Coty";
+  // var LastName = "Condry";
+
+  var ChatEntryPointId = session.entryPointId
+  var PhoneNumber = session.phone
+  var EmailAddress = session.email
+  var FirstName = session.firstName
+  var LastName = session.lastName
   var customerObject = null;
 
-  session.escalated = true
   customerObject =  new myLibrary.Datatype.CustomerObject();
   customerObject.SetPrimaryKey(customerObject.PrimaryKeyParams.PRIMARY_KEY_EMAIL, EmailAddress);
-
-
 
   var customerFirstName = new myLibrary.Datatype.CustomerParameter();
   customerFirstName.eGainParentObject = "casemgmt";
@@ -407,8 +361,148 @@ function escalateIt (session) {
   //myChat = myLibrary.myChat;
   /* Next get the event handlers for chat. It is mandatory to provide definition for the mandatory event handlers before initializing chat */
   //myEventHandlers = myChat.GetEventHandlers();
-  myChat.Initialize(ChatEntryPointId, 'en', 'US', myEventHandlers, 'aqua', 'v11');
-  /*Now set the customer */
-  myLibrary.SetCustomer(customerObject);
-  myChat.Start();
+
+  try {
+    /* Now create an instance of the Chat Object */
+    const myChat = new myLibrary.Chat();
+    /* Next get the event handlers for chat. It is mandatory to provide definition for the mandatory event handlers before initializing chat */
+    const myEventHandlers = createEventHandlers(myChat, session)
+
+    myChat.Initialize(ChatEntryPointId, 'en', 'US', myEventHandlers, 'aqua', 'v11');
+    /*Now set the customer */
+    myLibrary.SetCustomer(customerObject);
+    myChat.Start()
+    // add reference to ECE session in the session global
+    session.eceSession = myChat
+    session.escalated = true
+  } catch (e) {
+    console.error('error starting ECE chat', e)
+  }
+}
+
+function createEventHandlers (myChat, session) {
+  let myEventHandlers = myChat.GetEventHandlers()
+  /* Example browser alert when chat is connected */
+  myEventHandlers.OnConnectSuccess = function (args) {
+    console.log('OnConnectSuccess', args)
+    var welcomeMessage = "You are now connected to a Cumulus Financial Expert.";
+    console.log("You are now connected to an Agent " + welcomeMessage);
+    session.messages.push({
+      text: welcomeMessage,
+      type: 'system',
+      datetime: new Date().toJSON()
+    })
+  };
+  /* Example browser alert when there is a connection failure */
+  myEventHandlers.OnConnectionFailure = function (args) {
+    console.log('OnConnectionFailure', args)
+    // console.log('Oops! Something went wrong');
+  };
+  /* Example output of agent messages to a DIV named TransScript with jQuery */
+  myEventHandlers.OnAgentMessageReceived = function (args) {
+    console.log('OnAgentMessageReceived', args)
+    console.log("Agent Message Received: " + args.Message);
+    session.messages.push({
+      text: args.Message,
+      type: 'agent',
+      datetime: new Date().toJSON()
+    })
+  };
+  /* Example output of system messages to the same DIV */
+  myEventHandlers.OnSystemMessageReceived = function (args) {
+    console.log("System Message Received: " + args.Message)
+    session.messages.push({
+      text: args.Message,
+      type: 'system',
+      datetime: new Date().toJSON()
+    })
+  }
+  /* Example browser console.log when an error occurs */
+  myEventHandlers.OnErrorOccurred = function (args) {
+    console.log('Oops! Error Occurred' + args.toString());
+    session.messages.push({
+      text: args.toString(),
+      type: 'system',
+      datetime: new Date().toJSON()
+    })
+  };
+  /* Example browser console.log when agents are not available */
+  myEventHandlers.OnAgentsNotAvailable = function (args) {
+    console.log('Sorry no agents available', args);
+    session.messages.push({
+      text: args.toString(),
+      type: 'system',
+      datetime: new Date().toJSON()
+    })
+  };
+  /* Example browser console.log when the chat is completed */
+  myEventHandlers.OnConnectionComplete = function (args) {
+    console.log("Connection Complete!", args)
+    // remove escalated flag
+    session.escalated = false
+  };
+  /* Example of adding message in transcript when customer attachment invite is sent to server */
+  myEventHandlers.OnCustomerAttachmentNotificationSent = function (args) {
+    console.log('OnCustomerAttachmentNotificationSent', args)
+    const message = "Waiting for agent to accept attachment"
+    session.messages.push({
+      text: message,
+      type: 'system',
+      datetime: new Date().toJSON()
+    })
+  }
+  /* Example of uploading attachment to chat server when agent accepts attachment invite */
+  myEventHandlers.OnAttachmentAcceptedByAgent = function (args) {
+    file.uniqueFileId = args.uniqueFileId
+    myChat.UploadAttachment(file, args.agentName)
+    session.messages.push({
+      text: 'agent has accepted attachment',
+      type: 'system',
+      datetime: new Date().toJSON()
+    })
+  };
+
+  /* Example of sending notification to chat server when customer accepts attachment invite */
+  myEventHandlers.OnAttachmentInviteReceived = function(args){
+    var acceptBtn = document.createElement('input');
+    acceptBtn.type = "button";
+    acceptBtn.value = "Accept";
+    acceptBtn.addEventListener('click', function(){
+      sendAcceptChatAttachmentNotification(args.Attachment);
+    });
+    // $('#messages ul').append( '<li><span class="systemmsg-chat">' + args.Attachment.AgentName + " has sent attachment "+args.Attachment.Name + '</span><div class="clear"></div></li>');
+    // $('#messages ul').append(acceptBtn);
+  };
+
+  /* Example of downloading file when attachment is fetched from server */
+  myEventHandlers.OnGetAttachment = function(AgentAttachmentArgs){
+    if (typeof fileName !== 'undefine' && fileName !== null) {
+      if ((/\.(gif|jpg|jpeg|tiff|png)$/i).test(fileName)) {
+        myChat.GetAttachmentImage(AgentAttachmentArgs.fileId,AgentAttachmentArgs.uniqueFileId);
+      }
+      else{
+        var data = AgentAttachmentArgs.data;
+        var blob = new Blob([data]);
+        url = window.URL || window.webkitURL;
+        var fileUrl = url.createObjectURL(blob);
+        window.open(fileUrl);
+      }
+    }
+
+  };
+  /* Example of downloading file when attachment thumbnail is fetched from server */
+  myEventHandlers.OnGetAttachmentImageThumbnail = function(thumbnailArgs){
+    var thumbnailElement = document.createElement('img');
+    thumbnailElement.src = thumbnailArgs.data;
+    $('#messages ul').append("<br />");
+    $('#messages ul').append(thumbnailElement);
+  };
+
+  function sendAcceptChatAttachmentNotification(attachment){
+    fileName = attachment.Name;
+    myChat.SendAcceptChatAttachmentNotification(attachment.Id,attachment.Name);
+    myChat.GetAttachment(attachment.Id);
+  };
+
+  return myEventHandlers
 }
