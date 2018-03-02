582000d34a53516f678ef53307e769ba9f2c25c3
Coty Condry <ccondry@cisco.com>
2018-03-02 02:34:34 -0600

    don't log raw twilio webhook event

--- src/routes/twilio.js
+++ src/routes/twilio.js
@@ -4,7 +4,7 @@ const twilio = require('../twilio')
 
 // Accepts POST requests at /webhook endpoint for Facebook
 router.post('/', (req, res) => {
-  console.log("Twilio webhook event:" + JSON.stringify(req.body))
+  // console.log("Twilio webhook event:" + JSON.stringify(req.body))
   const message = req.body.Body
   const to = req.body.To
   const from = req.body.From
