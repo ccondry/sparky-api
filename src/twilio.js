b19c98225097a8b30ce5cdc36cbad92e57fa15f9
Coty Condry <ccondry@cisco.com>
2018-04-11 13:11:21 -0500

    clean up old brandConfig code - just use default values for aiToken and entry point ID try to get customer email, first name, and last name from dCloud answers API

--- src/twilio.js
+++ src/twilio.js
@@ -14,25 +14,6 @@ const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
 
 const contextService = require('./context-service')
 
-async function getBrandConfig (vertical) {
-  console.log('getting brand config for', vertical)
-  try {
-    const verticalConfig = await getVerticalConfig(vertical)
-    return verticalConfig.chat
-  } catch (e) {
-    return {}
-  }
-}
-
-// get dCloud session information
-function getVerticalConfig (vertical) {
-  return request({
-    method: 'GET',
-    url: `https://mm.cxdemo.net/api/v1/verticals/${vertical}`,
-    json: true
-  })
-}
-
 // get dCloud session information
 function getDcloudSession (from) {
   console.log('getting dcloud session info for', from)
@@ -56,13 +37,16 @@ function getDcloudSession (from) {
 }
 
 // get dCloud session information
-// function getDcloudSessionInfo (session, datacenter) {
-//   return request({
-//     method: 'GET',
-//     url: `https://mm.cxdemo.net/api/v1/datacenters/${datacenter}/sessions/${session}`,
-//     json: true
-//   })
-// }
+function getAnswers (phone) {
+  return request({
+    method: 'GET',
+    url: `https://mm.cxdemo.net/api/v1/answers/${phone}`,
+    headers: {
+      'Authorization': `Bearer ${process.env.DCLOUD_API_TOKEN}`
+    },
+    json: true
+  })
+}
 
 // send twilio SMS to user
 function sendMessage(from, to, body) {
@@ -152,33 +136,18 @@ async function handleMessage (message) {
       console.error('Error getting dCloud phone number registration info', e.message)
     }
 
-    // let dcloudSessionInfo = {}
-    // try {
-    //   // get dCloud session information
-    //   dcloudSessionInfo = getDcloudSessionInfo(dcloudSession.session, dcloudSession.datacenter)
-    // } catch (e) {
-    //   console.error('Error getting dCloud session info', e)
-    // }
-    const vertical = dcloudSession.vertical
-
-    let brandConfig = {}
-    let botConfig = {}
+    let answers = {}
+    let firstName = undefined
+    let lastName = undefined
+    let email = undefined
     try {
-      brandConfig = await getBrandConfig(vertical)
-      console.log('found brand config:', brandConfig)
-      botConfig = brandConfig.bot || {}
-      console.log('found bot config:', botConfig)
+      // get dCloud answers information that user submitted (hopefully)
+      answers = await getAnswers(phone)
+      firstName = answers.userName.split(' ')[0]
+      lastName = answers.userName.substring(firstName.length)
+      email = answers.emailAddress
     } catch (e) {
-      // continue
-    }
-    let botEnabled = true
-    if (botConfig.enabled === false) {
-      botEnabled = false
-    }
-    // enable survey by default
-    let survey = true
-    if (brandConfig.survey === false) {
-      survey = false
+      console.error('Error getting dCloud session info', e)
     }
 
     // create session and store in sessions global
@@ -187,15 +156,15 @@ async function handleMessage (message) {
       to,
       from,
       phone,
-      email: customerData.email || phone,
-      firstName: customerData.firstName || phone,
-      lastName: customerData.lastName || '',
-      apiAiToken: botConfig.aiToken,
-      entryPointId: brandConfig.entryPointId,
+      email: email || customerData.email || phone,
+      firstName: firstName || customerData.firstName || phone,
+      lastName: lastName || customerData.lastName || '',
+      // apiAiToken: botConfig.aiToken,
+      // entryPointId: brandConfig.entryPointId,
       dcloudSession: dcloudSession.session,
       dcloudDatacenter: dcloudSession.datacenter,
-      botEnabled,
-      survey,
+      botEnabled: true,
+      survey: true,
       onAddMessage: async function (type, message) {
         // send messages to SMS user, and decode HTML characters
         try {
