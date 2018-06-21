1b4733ccba2bb1bdbedf31010d6c89caebe1ece5
Coty Condry <ccondry@cisco.com>
2018-06-20 22:38:18 -0500

    try using a constant reference to twilio backup number env var

--- src/twilio.js
+++ src/twilio.js
@@ -11,7 +11,7 @@ const sessions = {}
 
 const twilio = require('twilio')
 const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
-
+const backupNumber = process.env.TWILIO_BACKUP_NUMBER
 const contextService = require('./context-service')
 
 function getLookupNumber (from, to) {
@@ -180,13 +180,13 @@ async function handleMessage (message) {
           // console.log('smsResponse', smsResponse)
           console.log(`SMS sent to ${from}`)
         } catch (e) {
-          console.error(`failed to send SMS to customer ${this.data.from} using ${this.data.to}. Retrying with backup number ${process.env.TWILIO_BACKUP_NUMBER}`)
+          console.error(`failed to send SMS to customer ${this.data.from} using ${this.data.to}. Retrying with backup number ${backupNumber}`)
           console.log(`this.to = ${this.data.to}. this.from = ${this.data.from}`)
           // unavailable using the current number?
           if (e.status === 400 && e.code === 21612) {
             // update this session to use the backup US number
             try {
-              updateSessionTo(session, process.env.TWILIO_BACKUP_NUMBER)
+              updateSessionTo(session, backupNumber)
               // try again
               const smsResponse2 = await sendMessage(this.data.to, this.data.from, entities.decode(message))
             } catch (e2) {
