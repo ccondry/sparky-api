9b40da0f42d87cb7a2425031cd89481a44be11ec
Coty Condry <ccondry@cisco.com>
2018-08-09 12:46:28 -0500

    log if the teams webhook was validated or not

--- src/routes/teams.js
+++ src/routes/teams.js
@@ -6,11 +6,13 @@ const spark = require('../spark')
 
 // webex teams webhook receiver
 router.post('/webhook', (req, res) => {
-  console.log(`Cisco Webex Teams webhook event on webhook ID ${req.body.id}`, req.body, req.headers)
+  console.log(`Cisco Webex Teams webhook event on webhook ID ${req.body.id}`)
   if (validateRequest(req, process.env.TEAMS_PAYLOAD_SECRET)) {
+    console.log('Webex Teams webhook event validated.')
     spark.handleWebhook(req.body).catch(e => console.error(e))
     return res.status(202).send()
   } else {
+    console.log('Webex Teams webhook event failed validation. Returning 401.')
     return res.status(401).send()
   }
 })
