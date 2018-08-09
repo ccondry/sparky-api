145aece873f6dc7e6675aa0adc2c1e65c4eae1d3
Coty Condry <ccondry@cisco.com>
2018-08-09 17:31:18 -0500

    update comment

--- src/routes/twilio.js
+++ src/routes/twilio.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const twilio = require('../twilio')
 
-// Accepts POST requests at /webhook endpoint for Facebook
+// SMS webhooks from twilio
 router.post('/', (req, res) => {
   // console.log("Twilio webhook event:" + JSON.stringify(req.body))
   const message = req.body.Body
