31128730c36d643018faa284b65f83a12f0ba2b9
Coty Condry <ccondry@cisco.com>
2018-09-04 16:06:49 -0500

    update localizations for pt_BR

--- src/localization.js
+++ src/localization.js
@@ -15,17 +15,17 @@ module.exports = {
   },
   pt_BR: {
     welcomeMessage: "Um momento, vou procurar um agente da Ativos disponível",
-    errorMessage: "I'm sorry, but we're having trouble connecting to an agent. Please try again later.",
-    waitingAcceptAttachment: "Waiting for agent to accept attachment",
-    acceptedAttachment: "agent has accepted attachment",
-    attachmentReceived: "has sent you a file",
-    sessionExpired: "Your previous session has ended.",
-    botFileAttachment: "I'm sorry, but I can't handle file attachments. If you would like to speak to an agent, say 'agent'.",
-    noAgentsAvailable: "Sorry, there are no agents available to assist you at this time. Please try again later.",
-    calculatorAppeared: "'Ok... Your calculator should have appeared on the left!'",
-    calculator: "Here is our mortgage calculator:",
-    noVideo: "I'm sorry, I'm not able to connect a video call to you from here.",
-    ccxPleaseWait: "Please wait while we connect you with a customer care representative..."
+    errorMessage: "Desculpe, estou com dificuldades em encontrar um atendente. Por favor tente mais tarde",
+    waitingAcceptAttachment: "aguardando o agente aceitar o anexo",
+    acceptedAttachment: "agente aceitou o anexo",
+    attachmentReceived: "lhe enviou um arquivo",
+    sessionExpired: "Sua sessão anterior foi fianlizada.",
+    botFileAttachment: "Desculpe, não posso aceitar anexos. Se deseja falar com um agente diga 'agente'.",
+    noAgentsAvailable: "Desculpe, não temos atendentes para lhe ajudar agora. Por favor, tente mais tarde.",
+    calculatorAppeared: "'Ok... uma calculadora vai aparecer aqui ao lado!'",
+    calculator: "Aqui está sua calculadora:",
+    noVideo: "Desculpe, não posso lhe conectar a uma video chamada.",
+    ccxPleaseWait: "Um momento, vou procurar um agente da Ativos disponível..."
   },
   es_MX: {
     welcomeMessage: "Ok, I'll get you connected to one of our experts. One moment...",
