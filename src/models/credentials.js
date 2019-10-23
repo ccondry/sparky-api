fa308cdf7093b28e60b1a44039d06c407ecf3b7a
Coty Condry <ccondry@cisco.com>
2019-10-23 15:12:51 -0500

    validate input

--- src/models/credentials.js
+++ src/models/credentials.js
@@ -3,6 +3,9 @@ const db = require('./db')
 
 module.exports = {
   get (project_id) {
+    if (!project_id || !project_id.length) {
+      throw new Error('GCP project ID required to retrieve credentials but was not provided.')
+    }
     // get GCP credentials JSON from database, key on project_id
     const query = {
       project_id
@@ -10,6 +13,9 @@ module.exports = {
     return db.findOne('toolbox', 'credentials', query)
   },
   set (credentials) {
+    if (!credentials || !credentials.project_id || !credentials.project_id.length) {
+      throw new Error('GCP credentials must contain a project_id property.')
+    }
     // update or insert GCP credentials JSON into database, key on project_id
     const query = {
       project_id: credentials.project_id
