1e5767477a85238941ad6472e58d8520760ed9bb
Coty Condry <ccondry@cisco.com>
2019-10-30 10:00:07 -0500

    rename test.js

---
+++ test/gcp-keyfile.js
@@ -0,0 +1,14 @@
+// Load our environment variables
+require('dotenv').load()
+// connect to database, log any errors
+const db = require('../src/models/db')
+const fs = require('fs')
+const keyfile = fs.readFileSync('./credentials/cumulus-v2-hotikl-e942c76ea50b.json', 'utf8')
+console.log(keyfile)
+const credentials = JSON.parse(keyfile)
+db.connect()
+.then(c => {
+  db.insertOne('toolbox', 'credentials', credentials)
+  .then(r => console.log('credentials saved to mongo.'))
+  .catch(e => console.error('failed to save credentials to mongo:', e))
+})
--- test/test.js
+++
@@ -1,4 +0,0 @@
-let d = new Date()
-console.log('d =', d)
-d.setSeconds(d.getSeconds() + 300)
-console.log('d =', d)
