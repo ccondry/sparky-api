b87feff9ab355d4fef36746c359063ceb2fed560
Coty Condry <ccondry@cisco.com>
2020-05-04 08:55:29 -0500

    validate onTypingStart and onTypingStart are functions before trying to run them

--- src/smEventHandlers.js
+++ src/smEventHandlers.js
@@ -39,11 +39,15 @@ function create (session) {
     },
     onTypingStart (from) {
       console.log(session.id, from, 'is typing')
-      session.onTypingStart(from)
+      if (typeof session.onTypingStart === 'function') {
+        session.onTypingStart(from)
+      }
     },
     onTypingStop (from) {
       console.log(session.id, from, 'stopped typing')
-      session.onTypingStop(from)
+      if (typeof session.onTypingStop === 'function') {
+        session.onTypingStop(from)
+      }
     },
     onOtherEvent (type, ev) {
       console.log(session.id, 'other event', type, ev)
